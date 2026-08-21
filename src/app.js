64421ddafb0164a04d3d2524b921ec4545f42f35
Átila Rodrigues <atila.rodriguesal@gmail.com>
2026-08-21 17:06:37 -0300

    feat: add glob pattern routing and link interception

--- src/app.js
+++ src/app.js
@@ -1,16 +1,4 @@
 var engine = new Engine({
-    routes: {
-        '/': {
-            route: '/index.html',
-            enabled: true,
-        },
-        '/about': {
-            route: '/routes/about.html',
-            enabled: true,
-        },
-        '/no-spa-nav': {
-            route: '/routes/no-spa-nav.html',
-            enabled: false,
-        },
-    },
+    routes: ['*.html', '/routes/*'],
+    enabled: true,
 })
