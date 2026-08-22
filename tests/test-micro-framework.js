5b30416a05331352a836d1404bbe9cf6e3f2b1cf
atilara <atila.rodriguesal@gmail.com>
2026-08-22 14:26:09 -0300

    test: create micro framework for testing with assertion functions

---
+++ tests/test-micro-framework.js
@@ -0,0 +1,37 @@
+export function it(description, testFunction) {
+    const result = document.createElement('p')
+    result.classList.add('test_result')
+
+    try {
+        testFunction()
+        result.classList.add('success')
+        result.innerHTML = description
+    } catch (error) {
+        result.classList.add('failure')
+        result.innerHTML = `${description}<br/><pre>${error}</pre>`
+    }
+
+    document.body.appendChild(result)
+}
+
+export function assertEqual(x, y) {
+    if (
+        x === y ||
+        (typeof x === 'object' &&
+            typeof y === 'object' &&
+            x.length === y.length &&
+            x.every((element, index) => element === y[index]))
+    ) {
+        return
+    } else {
+        throw new Error(`${x} != ${y}`)
+    }
+}
+
+export function assertTrue(x) {
+    assertEqual(x, true)
+}
+
+export function assertFalse(x) {
+    assertEqual(x, false)
+}
